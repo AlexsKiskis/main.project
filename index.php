49b6b9d2cfec1e8c8a95a36066cee551fc5d79f0
AlexsKiskis <20300215@live.inueco.ru>
2023-04-17 15:16:51 +0500

    create structur OOP

--- index.php
+++ index.php
@@ -1,52 +1,55 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Клиент-серверное приложение</title>
-</head>
-<body>
-
-<form action="insertStudent.php" method="POST">
-<input type="text" name="fname" id="fname" placeholder="Введите имя" required><br>
-<input type="text" name="lname" id="lname" placeholder="Введите фамилию" required><br>
-<input type="number" name="age" id="age" placeholder="Ваш возраст" required><br>
-<input type="radio" name="gender" id="m" value="m" checked>
-<label for="m">Мужской</label>
-<input type="radio" name="gender" id="f" value="f">
-<label for="f">Женский</label><br>
-<input type="submit" value="Добавить">
-</form>
-
-
-
 <?php
+header("Content-Type:text/html; charset=UTF-8");
 
-require_once("config.php");
+require_once("api/config.php");
+require_once("api/core.php");
 
-//соединение с БД
-$connect = new mysqli(HOST, USER, PASSWORD, DB);
-if($connect->connect_error){
-    exit("Ошибка подключения к БД: ".$connect->connect_error);
+
+if(file_exists("api/main.php")){
+    //проверка сощуствования файла
+    include("api/main.php"); //подключаем 
+    if (class_exists("Main")){
+        $obj  = new Main; //создаем объект от класса мейн
+        $obj->get_body();
+    } else {
+        exit("<p>Не верный класс</p>");
+    }
 }
-//установить кодировку
-$connect->set_charset("utf8");
+    else {
+        exit("<p>Не верный путь</p>");
+    }
 
-//код запроса
-$sql = "SELECT * FROM `students`";
 
-//выполнить запрос
-$result = $connect->query($sql);
 
-//вести результаты запроса на страницу
-while ($row = $result->fetch_assoc()){
-    echo "<div>
-            $row[lname], $row[fname], $row[gender], $row[age]
-        </div>";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
-    
-</body>
-</html>
